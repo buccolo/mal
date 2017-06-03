df8b670475d3ebfc6d5231beb471931f777691b4
Bruno Buccolo <bruno.buccolo@gmail.com>
2017-06-03 11:11:45 -0300

    Bootstraps Horcrux files with .exs

---
+++ horcrux/lib/horcrux.ex
@@ -0,0 +1,18 @@
+defmodule Horcrux do
+  @moduledoc """
+  Documentation for Horcrux.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Horcrux.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ horcrux/test/horcrux_test.exs
@@ -0,0 +1,8 @@
+defmodule HorcruxTest do
+  use ExUnit.Case
+  doctest Horcrux
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
